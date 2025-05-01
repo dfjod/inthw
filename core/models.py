fc3455557c3c8adaa46ba497e425380f7d561b72
Davis Fjodorovs <121906325+dfjod@users.noreply.github.com>
2025-05-01 21:34:08 +0300

    Add core app structure and initial models and migrations

---
+++ core/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    label = models.CharField(max_length=35)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.label
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class DataPoint(models.Model):
+    label = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.label
+
+class Object(models.Model):
+    label = models.CharField(max_length=70)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    responsible_persons = models.ManyToManyField(Person)
+    data_points = models.ManyToManyField(DataPoint)
+
+    def __str__(self):
+        return f'Project: {self.project}, Label: {self.label}, Responsible persons: {self.responsible_persons}, Data points: {self.data_points}'
