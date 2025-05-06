e8245113756ebcfe2a56e24267b4386d9842b586
Davis Fjodorovs <121906325+dfjod@users.noreply.github.com>
2025-05-06 11:04:06 +0300

    Add API versioning and update URL paths to include version prefix

--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("clients/", views.ClientListCreate.as_view(), name="client_view_create"),
-    path("projects/", views.ProjectListCreate.as_view(), name="project_view_create"),
-    path("persons/", views.PersonListCreate.as_view(), name="person_view_create"),
-    path("datapoints/", views.DataPointListCreate.as_view(), name="datapoint_view_create"),
-    path("objects/", views.ProjectObjectListCreate.as_view(), name="object_view_create"),
-    path("clients/<int:pk>/", views.ClientRetrieveUpdateDestroy.as_view(), name="client_view_retrieve_udpate_destroy"),
-    path("projects/<int:pk>/", views.ProjectRetrieveUpdateDestory.as_view(), name="project_view_retrieve_udpate_destroy"),
-    path("persons/<int:pk>/", views.PersonRetrieveUpdateDestory.as_view(), name="person_view_retrieve_udpate_destroy"),
-    path("datapoints/<int:pk>/", views.DataPointRetrieveUpdateDestory.as_view(), name="datapoint_view_retrieve_udpate_destroy"),
-    path("objects/<int:pk>/", views.ProjectObjectRetrieveUpdateDestory.as_view(), name="object_view_retrieve_udpate_destroy"),
+    path("v1/clients/", views.ClientListCreate.as_view(), name="client_view_create"),
+    path("v1/projects/", views.ProjectListCreate.as_view(), name="project_view_create"),
+    path("v1/persons/", views.PersonListCreate.as_view(), name="person_view_create"),
+    path("v1/datapoints/", views.DataPointListCreate.as_view(), name="datapoint_view_create"),
+    path("v1/objects/", views.ProjectObjectListCreate.as_view(), name="object_view_create"),
+    path("v1/clients/<int:pk>/", views.ClientRetrieveUpdateDestroy.as_view(), name="client_view_retrieve_udpate_destroy"),
+    path("v1/projects/<int:pk>/", views.ProjectRetrieveUpdateDestory.as_view(), name="project_view_retrieve_udpate_destroy"),
+    path("v1/persons/<int:pk>/", views.PersonRetrieveUpdateDestory.as_view(), name="person_view_retrieve_udpate_destroy"),
+    path("v1/datapoints/<int:pk>/", views.DataPointRetrieveUpdateDestory.as_view(), name="datapoint_view_retrieve_udpate_destroy"),
+    path("v1/objects/<int:pk>/", views.ProjectObjectRetrieveUpdateDestory.as_view(), name="object_view_retrieve_udpate_destroy"),
 ]
